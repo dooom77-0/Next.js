6553f2a088c1ba5627e491c8b503fb5a5225df12
اسمك هنا < >
2025-12-15 13:29:13 +0300

    ..

--- my-projects/app/page.js
+++ my-projects/app/page.js
@@ -1,20 +1,31 @@
 import Image from "next/image"
 export default function Projects() {
   const projects = [
-    { id: 1,img:"/ayat.jpg", name: "عارض الآيات", des: "تطبيق يعرض آيات من القرآن الكريم بطريقة منظمة، مع تصميم بسيط يساعد على التأمل والقراءة.", url: "https://random-ayat-d7.pages.dev/",tags:"Html Css JS" },
+    { id: 1, img: "/ayat.jpg", name: "عارض الآيات", des: "تطبيق يعرض آيات من القرآن الكريم بطريقة منظمة، مع تصميم بسيط يساعد على التأمل والقراءة.", url: "https://random-ayat-d7.pages.dev/", tags: "Html Css JS API" },
+    
     {
       id: 2, img: "/sunnah.jpg", name: "تطبيق السنة", des: "يعرض أحاديث نبوية مختارة، مع واجهة سهلة للتنقل بين الأحاديث، ويهدف لنشر السنة بأسلوب عصري.", url: "https://sunnah-app-d7.pages.dev/",
-      tags:"Html Css JS React"
-     },
-    { id: 3, img:"/Old.png", name: "Old portfolio", des: "أول مشروع لي باستخدام html وcss", url: "https://my-own-site-k7k.pages.dev/", tags:"Html Css" },
-    { id: 4, img:"/Memory.jpg", name: "لعبة الذاكرة", des: "لعبة تفاعلية تساعد على تحسين الذاكرة والتركيز.", url: "https://memory-game-d7.pages.dev/" ,tags:"Html Css JS"},
-    { id: 5, img:"/Guess.jpg", name: "لعبة تخمين الرقم", des: "لعبة ممتعة لتخمين الرقم المُختار من قبل الحاسوب.", url: "https://guess-number-d7.pages.dev/", tags:"Html Css JS" },
-    { id: 6, img:"/todo.jpeg", name: "قائمة المهام", des: "تطبيق يساعد على تنظيم المهام اليومية بسهولة.", url: "https://to-do-list-d7.pages.dev/", tags:"Html Css JS React" },
-    { id: 7, img:"/weather.jpg", name: "تطبيق الطقس", des: "يعرض معلومات الطقس الحالية والتنبؤ بالطقس في مختلف المدن.", url: "https://weather-app-d7.pages.dev/", tags:"Html Css JS" },
-    { id: 8,img:"/earth.jpg", name: "معلومات الدول", des: "يعرض معلومات شاملة عن الدول المختلفة حول العالم.", url: "https://countries-info-d7.pages.dev/", tags:"Html Css API" },
-    { id: 9, img:"/currency.jpg", name: "محول العملات", des: "يقوم بتحويل مبلغ من وحدة العملات الى وحدة اخرى", url: "https://currency-converter-d7.pages.dev/", tags:"Html Css API" },
-    { id: 10, img:"/New.png", name: "New portfolio", des: "موقعي الشخصي الجديد باستخدام react js", url: "https://potrfolio-d7.pages.dev/" },
-    { id: 11, img:"/time.jpg", name: "مركز الوقت", des: "يعرض الوقت الحالي في مختلف المدن حول العالم.", url: "https://time-hub-d7.vercel.app/", tags:"Html Css Next js" },
+      tags:"Html Css JS React JSON"
+    },
+     
+    { id: 3, img: "/Old.png", name: "Old portfolio", des: "أول مشروع لي باستخدام html وcss", url: "https://my-own-site-k7k.pages.dev/", tags: "Html Css" },
+    
+    { id: 4, img: "/Memory.jpg", name: "لعبة الذاكرة", des: "لعبة تفاعلية تساعد على تحسين الذاكرة والتركيز.", url: "https://memory-game-d7.pages.dev/", tags: "Html Css JS" },
+    
+    { id: 5, img: "/Guess.jpg", name: "لعبة تخمين الرقم", des: "لعبة ممتعة لتخمين الرقم المُختار من قبل الحاسوب.", url: "https://guess-number-d7.pages.dev/", tags: "Html Css JS" },
+    
+    { id: 6, img: "/todo.jpeg", name: "قائمة المهام", des: "تطبيق يساعد على تنظيم المهام اليومية بسهولة.", url: "https://to-do-list-d7.pages.dev/", tags: "Html Css JS React" },
+    
+    { id: 7, img: "/weather.jpg", name: "تطبيق الطقس", des: "يعرض معلومات الطقس الحالية والتنبؤ بالطقس في مختلف المدن.", url: "https://weather-app-d7.pages.dev/", tags: "Html Css JS API" },
+    
+    { id: 8, img: "/earth.jpg", name: "معلومات الدول", des: "يعرض معلومات شاملة عن الدول المختلفة حول العالم.", url: "https://countries-info-d7.pages.dev/", tags: "Html Css JS API" },
+    
+    { id: 9, img: "/currency.jpg", name: "محول العملات", des: "يقوم بتحويل مبلغ من وحدة العملات الى وحدة اخرى", url: "https://currency-converter-d7.pages.dev/", tags: "Html Css JS API" },
+    
+    { id: 10, img: "/New.png", name: "New portfolio", des: "موقعي الشخصي الجديد باستخدام react js", url: "https://portfolio-d7.pages.dev/", tags: "Html Css JS React" },
+    
+    { id: 11, img: "/time.jpg", name: "مركز الوقت", des: "يعرض الوقت الحالي في مختلف المدن حول العالم.", url: "https://time-hub-d7.vercel.app/", tags: "Html Css JS Nextjs" },
+    
   ]
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-screen p-8 
---
+++ news-project/app/page.js
@@ -0,0 +1,8 @@
+// import NewsClient from "./components/NewsClient.jsx";
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center w-screen ">
+      {/* <NewsClient /> */}
+    </div>
+  );
+}
